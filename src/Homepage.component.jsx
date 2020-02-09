f9bd75c75900cb0b282f93f169383242d89b808d
Zelal EizAldeen <zelalezaldeen@gmail.com>
2020-02-09 22:12:27 +0300

    created flex fitness with react

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage = ( )=> (
+    <div className='homepage'>
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Cardio</h1>
+                <span className='subtitle'>Add Program</span>
+
+            </div>
+        </div>
+
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Resistance</h1>
+                <span className='subtitle'>Add Program</span>
+
+            </div>
+        </div>
+
+        </div>
+    </div>
+);
+export default Homepage;
